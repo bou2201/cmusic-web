90aac2bcdf58ded2efd35319302e1831f5f381bf
bou2201 <lamchuc123456@gmail.com>
2025-03-02 11:19:03 +0700

    ci(config): :wrench: update `prettierc`, `eslint` and next.config

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,17 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  /* Config options here */
+
+  /* Allow all patterns */
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: '**',
+      },
+    ],
+  },
 };
 
 export default nextConfig;

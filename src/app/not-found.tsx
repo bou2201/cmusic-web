2edb74ed5720fe715b6f4848c77707bd373fc35a
bou2201 <lamchuc123456@gmail.com>
2025-03-05 23:30:54 +0700

    perf(page): add not-found page

---
+++ src/app/not-found.tsx
@@ -0,0 +1,18 @@
+'use client';
+
+import { routing } from '@/i18n/routing';
+import Error from 'next/error';
+
+// This page renders when a route like `/unknown.txt` is requested.
+// In this case, the layout at `app/[locale]/layout.tsx` receives
+// an invalid value as the `[locale]` param and calls `notFound()`.
+
+export default function GlobalNotFound() {
+  return (
+    <html lang={routing.defaultLocale}>
+      <body>
+        <Error statusCode={404} />;
+      </body>
+    </html>
+  );
+}
